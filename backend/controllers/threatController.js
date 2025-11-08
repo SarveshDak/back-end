3ac609e6c689bee6cf615183bcc21f32b4f94cfd
Sarvesh Dak <sarveshdak13@gmail.com>
2025-11-08 21:48:22 +0530

    Fixed JSON response + return correct object

--- backend/controllers/threatController.js
+++ backend/controllers/threatController.js
@@ -1,5 +1,5 @@
 import { fetchThreatData } from "../utils/fetchThreats.js";
 export const fetchThreats = async (req, res) => {
   const result = await fetchThreatData();
-  res.json(result);
+  res.json(formattedData);
 };
